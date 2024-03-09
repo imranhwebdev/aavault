18785cb1a2f85ddd09450a50d1156a82ea7e4acb
imranhwebdev <alimrancv870@gmail.com>
2024-03-09 22:52:29 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Banner from './components/Banner';
 import About from './components/About';
 import OurTeam from './components/OurTeam';
+import Faq from './components/Faq';
 import Footer from './components/Footer'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <About />
       <OurTeam />
+      <Faq />
       <Footer />
     </>
   )
---
+++ src/components/Faq.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
+export default function Faq() {
+    const title = "FREQUENTLY ASKED <span>QUESTIONS</span>";
+    const accordionData = [
+        {
+            id:1,
+            title:'Is your platform secure?',
+            desc:'Yes, our platform is designed with top-tier security measures to ensure your data and transactions remain safe and confidential.',
+        },
+        {
+            id:2,
+            title:'How safe is my data?',
+            desc:'Yes, our platform is designed with top-tier security measures to ensure your data and transactions remain safe and confidential.',
+        },
+        {
+            id:3,
+            title:'Will you have a token?',
+            desc:'Yes, our platform is designed with top-tier security measures to ensure your data and transactions remain safe and confidential.',
+        },
+        {
+            id:4,
+            title:'What blockchains does your platform support?',
+            desc:'Yes, our platform is designed with top-tier security measures to ensure your data and transactions remain safe and confidential.',
+        },
+    ]
+  return (
+    <section className='faqArea'>
+      <Container>
+        <Row>
+            <Col>
+                <div className="faqWrapper">
+                    <div className="section-title">
+                        <h2 dangerouslySetInnerHTML={{ __html: title }} />
+                    </div>
+                    <Accordion defaultActiveKey={accordionData[0].id.toString()}>
+                        {accordionData.map((item) => (
+                            <Accordion.Item key={item.id} eventKey={item.id.toString()}>
+                                <Accordion.Header>{item.title}</Accordion.Header>
+                                <Accordion.Body>{item.desc}</Accordion.Body>
+                            </Accordion.Item>
+                        ))}
+                    </Accordion>
+                </div>
+            </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
